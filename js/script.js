1c4d565a476a8ef463e737f048e5fee9e0a4c2ee
Jonathan Smith <jonathan@peoplesized.com>
2015-08-12 15:06:15 -0400

    basics

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$.getJSON('countries.json', function(data){
+  console.dir(data); 
+  doSummary(data);
+  return doFilters(data);
+})
+
+function doSummary(data) {
+  new Ractive({
+    el: '.summary',
+    template: '#summary',
+    data: { countries: data }
+  });
+}
+
+function doFilters(data) {
+  new Ractive({
+    el: '.filters',
+    template: '#filters',
+    data: { countries: data }
+  });
+}
