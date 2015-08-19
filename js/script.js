9bf29297e6c0ea6ce8814f9ce36e58960a39d902
Jonathan Smith <jonathan@peoplesized.com>
2015-08-19 12:02:38 -0400

    more

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@ $.getJSON('data/iif_status.json', function(data) {
   app.filtered_data = new FilteredCollection(app.data);
   app.explorer = explorer(app.filtered_data);
 
-  // app.explorer.on('change', function(changeObject) { 
-  //   if (changeObject.selectedCountry != undefined) {
-  //     if (changeObject.selectedCountry) { 
-  //       return console.log('change selectedCountry to', changeObject.selectedCountry);
-  //     } else {
-  //       return console.log('reset selectedCountry ');
-  //     }
-  //   };
-  //   return;
-  // });
+  app.explorer.on('change', function(changeObject) { 
+    // if (changeObject.selectedCountry != undefined) {
+    //   if (changeObject.selectedCountry) { 
+    //     return console.log('change selectedCountry to', changeObject.selectedCountry);
+    //   } else {
+    //     return console.log('reset selectedCountry ');
+    //   }
+    // };
+    return console.log('change list');
+  });
 
-  window.m = drawMap(app.data);
-  window.mo = m.vectorMap('get', 'mapObject');
+  map = drawMap(app.filtered_data);
+  app.map = map.vectorMap('get', 'mapObject');
   return;
 });
