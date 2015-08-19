a7b56ca3bf5a415667d02c02694d4f4259900676
Jonathan Smith <jonathan@peoplesized.com>
2015-08-19 14:16:00 -0400

    Country model

--- js/collections.js
+++ js/collections.js
@@ -1,9 +1,22 @@
 window.app || (window.app = {});
 
 // 
+// Models
+// 
+
+var Country = Backbone.Model.extend({
+  // initialize: function() {
+  // }
+})
+
+// 
 // Collections
 // 
 
 var Countries = Backbone.Collection.extend({
+  model: Country,
+  // initialize: function() {
+  //   return console.log('init collection');
+  // },
   comparator: 'country'
 });
