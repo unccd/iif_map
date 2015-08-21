0ebfaa5c92bccf9702b375137b1c5e2330c3336f
Jonathan Smith <jonathan@peoplesized.com>
2015-08-21 11:13:06 -0400

    additional filters defined - dummy

--- js/script.js
+++ js/script.js
@@ -7,20 +7,27 @@ window.app || (window.app = {});
 
 $.getJSON('data/iif_status.json', function(data) {
   // Backbone.Obscura collection for filtering
-  app.filtered_data = new Backbone.Obscura(new Countries(data));
+  app.data = new Backbone.Obscura(new Countries(data));
 
   // Possible Filters collection
-  app.filters = new Filters([
-    {attribute: '', title: 'All', short_name: 'all', filterState: {}, exclusive: true}, 
-    {attribute: 'iif_or_plan', title: 'With IIF', short_name: 'with_iif', filterState: {iif_or_plan: 'iif'}, exclusive: true}, 
-    {attribute: 'iif_or_plan', title: 'No IIF, plan exists', short_name: 'with_plan', filterState: {iif_or_plan: 'plan'}, exclusive: true}, 
-    {attribute: 'iif_or_plan', title: 'No IIF, no plan', short_name: 'no_plan', filterState: {iif_or_plan: 'no_plan'}, exclusive: true},
-    {attribute: 'iif_or_plan', title: 'No data', short_name: 'unknown', filterState: {iif_or_plan: 'unknown'}, exclusive: true},
-    {attribute: 'iif_or_plan', title: 'Planned 2014-2015', short_name: '2014_2015', filterState: {iif_plan_start: '2014_2015'}, exclusive: true},
-    {attribute: 'iif_or_plan', title: 'Planned 2016-2017', short_name: '2016_2017', filterState: {iif_plan_start: '2016_2017'}, exclusive: true},
-    {attribute: 'iif_or_plan', title: 'Planned 2018-2019', short_name: '2018_2019', filterState: {iif_plan_start: '2018_2019'}, exclusive: true},
-    {attribute: 'iif_or_plan', title: 'GM supported', short_name: 'gm_supported', filterState: {gm_supported: true}, exclusive: true}
-  ]);
+  app.filters = {}
+
+  app.filters.iif_status = new Filters([
+    {title: 'With IIF', id: 'with_iif', filterState: {iif_or_plan: 'iif'}}, 
+    {title: 'No IIF, plan exists', id: 'with_plan', filterState: {iif_or_plan: 'plan'}}, 
+    {title: 'No IIF, no plan', id: 'no_plan', filterState: {iif_or_plan: 'no_plan'}},
+    {title: 'No data', id: 'unknown', filterState: {iif_or_plan: 'unknown'}},
+    {title: 'Planned 2014-2015', id: '2014_2015', filterState: {iif_plan_start: '2014_2015'}},
+    {title: 'Planned 2016-2017', id: '2016_2017', filterState: {iif_plan_start: '2016_2017'}},
+    {title: 'Planned 2018-2019', id: '2018_2019', filterState: {iif_plan_start: '2018_2019'}},
+    {title: 'GM supported', id: 'gm_supported', filterState: {gm_supported: true}}
+  ], {type: 'iif_status'});
+
+  app.filters.geo = new Filters([
+    {title: 'Spain', id: 'spain', filterState: {iso3: 'spa'}},
+    {title: 'Europe', id: 'europe', filterState: {region: 'europe'}},
+    {title: 'Southern Europe', id: 'southern_europe', filterState: {subregion: 'southern_europe'}}
+  ], {type: 'geo'})
 
 
   // Init Ractive decorators
