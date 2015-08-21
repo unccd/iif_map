dfb84856b7ef499358a1679d0789fc1b8a2c825d
Jonathan Smith <jonathan@peoplesized.com>
2015-08-21 10:37:35 -0400

    browsersync and gulp. useful and fast.

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    open: false
+  });
+  gulp.watch("**/*", [], browserSync.reload);
+});
+
+// Default taks
+gulp.task('default', ['serve']);
